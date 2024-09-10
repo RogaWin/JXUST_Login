122b2064390f6558d09cf92057f0487b9fa9b359
Luojiawei <119515790+RogaWin@users.noreply.github.com>
2024-09-10 15:21:37 +0800

    Update loginWifi.py

--- loginWifi/loginWifi.py
+++ loginWifi/loginWifi.py
@@ -7,7 +7,7 @@ max_retries = 5
 #配置账号和密码
 your_account="你的账号";
 your_password="你的密码";
-
+your_mac="你的mac值可以在请求参数看 或者不代这个参数 浏览器会自动帮你填入"
 
 # 定义请求的URL和参数
 url = "http://10.17.8.18:801/eportal/portal/login"
@@ -18,7 +18,7 @@ params = {
     "user_password": os.getenv("USER_PASSWORD", your_password),  # 从环境变量读取，或使用默认值
     "wlan_user_ip": "10.21.102.241",
     "wlan_user_ipv6": "",
-    "wlan_user_mac": "a98450ed357c3e56ed8e8935f300576d71db97133f303b3b",
+    "wlan_user_mac": your_mac,
     "wlan_ac_ip": "10.17.4.1",
     "wlan_ac_name": "",
     "jsVersion": "4.1.3",
